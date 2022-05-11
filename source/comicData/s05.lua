ccf6effbe2f2c0084e71859775d147391716e615
Cadin Batrack <cabatrac@microsoft.com>
2022-05-10 22:01:27 -0700

    add s05 polish

--- source/comicData/s05.lua
+++ source/comicData/s05.lua
@@ -2,23 +2,25 @@ s05 = {
 	title = "3. Let There Be Light",
 	panels = {
 		{
-			-- bulb reveal
+			-- branches
 			layers = {
-				{ image = "s05/A-1a-bg.png", parallax = 1 },
-				{ image = "s05/A-2a-branch.png", parallax = 0.8 },
-		
+				{ image = "s05/AA-1-bg.png", parallax = 1 },
+				{ image = "s05/AA-2-branch1.png", parallax = 0.6 },
+				{ image = "s05/AA-3-branch2.png", parallax = 0.3 },
 			},
 		},
 		{
 			-- bulb reveal
+			
 			layers = {
-				{ images = {"s05/A-1a-bg.png", "s05/A-1b-bg.png"}, parallax = 1 },
-				{ images = {"s05/A-2a-branch.png", "s05/A-2b-branch.png"}, parallax = 0.8 },
+				{ images = {"s05/A-1a-bg.png","s05/A-1a-bg.png", "s05/A-1b-bg.png", "s05/A-1b-bg.png", "s05/A-1b-bg.png"}, parallax = 1 },
+				{ images = {"s05/A-2a-branch.png", "s05/A-2b-branch.png", "s05/A-2c-branch.png", "s05/A-2c-branch.png", "s05/A-2c-branch.png"}, parallax = 0.8 },
 
 			},
 		}, 
 		{
 			-- man admire
+			audio = { file = "s05/chimes", scrollTrigger = 0.1 },
 			layers = {
 				{ image = "s05/B-1-man.png", parallax = 1 },
 				{ image = "s05/B-2-hand.png", parallax = 0.9 },
@@ -35,8 +37,9 @@ s05 = {
 				{ image = "s05/C-3-branch1.png", parallax = 0.25, x = 120 },
 				{ image = "s05/C-4-branch2.png", parallax = 0.1, x = 50 },
 				{ image = "s05/C-5-logo.png", parallax = 0.5, x = 350 },
-				
-				-- { text = "A mini-comic featuring *The Botanist*", x = 1185, y = 200, parallax = 0.5, color = Panels.Color.WHITE},
+				{ image = "s05/C-6-branch3.png", parallax = 0.3, x = 888 },
+				{ image = "s05/C-7-panelsCredit.png", parallax = 0.2, x = 1860, y = 50},
+
 				{ text = "by *Cadin Batrack*", x = 1230, y = 120, parallax = 0.5, color = Panels.Color.WHITE},
 			},
 		}
