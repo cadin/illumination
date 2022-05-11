27dc72a889c72e141e8b2f9882177916102d3e80
Cadin Batrack <cabatrac@microsoft.com>
2022-05-10 21:22:12 -0700

    add s02 audio

--- source/comicData/s02.lua
+++ source/comicData/s02.lua
@@ -8,11 +8,11 @@ s02 = {
 		},
 		{
 			frame = { gap = 50, margin = 8, height = 960},
+			audio = { file = "s02/drips", loop = true, volume = 0.2},
 			layers = {
 				{ image = "s02/2-A-backWall.png", parallax = 0.3},
 				{ image = "s02/2-B-foreground.png", parallax = 0.2},
-				{ image = "s02/2-C-alerts.png", parallax = 0.1}
-				
+				{ image = "s02/2-C-alerts.png", parallax = 0.1}	
 			}
 		}
 	},
