2659d7eeb47e2234e3404518c3c12782ddb4e48c
Cadin Batrack <cabatrac@microsoft.com>
2022-05-10 21:20:28 -0700

    add audio and polish to s01

--- source/comicData/s01.lua
+++ source/comicData/s01.lua
@@ -2,8 +2,30 @@ s01 = {
 	title="1. Darkness Falls",
 	panels = {
 		{
+			-- EXT planet
+			font = "fonts/font-Cuberick-Bold",
+			layers = {
+				{ image = "s01/AA-0-starfield.png", parallax = 1 },
+				
+				{ image = "s01/AA-2-planet.png", parallax = 0.8 },
+				{ image = "s01/AA-1-planet.png", parallax = 0.5 },
+				{ image = "s01/AA-3-marker.png", parallax = 0.5, 
+					effect = { type = Panels.Effect.BLINK, durations = {on = 1000, off = 500} } 
+				},
+				{ text = "VENUSIAN OUTPOST 02", 
+					effect = { 
+						type = Panels.Effect.TYPE_ON, delay = 500
+					}, 
+					background = Panels.Color.BLACK,
+					x = 20, y = 190, 
+					parallax = 0.2
+				}
+			},
+		},
+		{
 			-- EXT station
 			frame = { width = 760, margin= 9, gap=50 },
+			audio = { file = "s01/bgLoop", loop = true},
 			layers = {
 				{ image = "s01/A-1-starfield.png", parallax = 1, x=20 },
 				{ image = "s01/A-2-mountains.png", parallax = 0.8, x = 50 },
@@ -14,24 +36,36 @@ s01 = {
 		},
 		{
 			-- light switch
+			transitionOffset = -0.025, -- trigger transition just before 50%
+			audio = { file = "s01/switch", scrollTrigger = 0.475 },
 			layers = {
 				{ images = { "s01/B-1a-roomDark.png", "s01/B-1b-roomLit.png" } , parallax = 1 },
 				{ image = "s01/B-2-wall.png", parallax = 0.8 },
-				{ images = { "s01/B-3a-switchOff.png", "s01/B-3b-switchOn.png" }, parallax = 0.8 },
+				{ images = { "s01/B-3a-switchOff.png", "s01/B-3b-switchOn.png" }, parallax = 0.8},
 				{ image = "s01/B-4-hand.png", parallax = 0.7, y = 50, x = -30, animate={y = -10, x = 10} },
 			},
 		},
 		{
 			-- man looking up
+			audio = { file = "s01/lightHum", loop = true},
 			layers = {
 				{ image = "s01/C-1-room.png", parallax = 1 },
 				{ image = "s01/C-2-wall.png", parallax = 0.9 },
 				{ image = "s01/C-3-doorway.png", parallax = 0.8 },
+				{ image = "s01/C-5-darken.png", parallax = 0.8, 
+					effect = {
+						type = Panels.Effect.BLINK, 
+						durations = {on = 50, off = 100}, 
+						reduceFlashingDurations = {on = 500, off = 2000}
+					}
+				},
 				{ image = "s01/C-4-man.png", parallax = 0.7 },
 			},
 		}, 
 		{
 			-- bulb burning out
+			transitionOffset = -0.025, -- trigger transition just before 50%
+			audio = { file = "s01/bulbPop", scrollTrigger = 0.475 },
 			layers = {
 				{ images = {"s01/D-1a-bulbOn.png", "s01/D-1b-bulbOff.png"}, parallax = 1 },
 				{ images = {"s01/D-2a-alerts.png", "s01/D-2b-bubbles.png"}, parallax = 0.9 },
